54d25addf32ae4e49f97786c0660a757dedec26e
yeyeyey <cjttkfkd3941@gmail.com>
2025-01-30 14:26:29 +0900

    [Silver II] Title: 인터뷰, Time: 136 ms, Memory: 44016 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27914. 인터뷰/인터뷰.py
+++ 백준/Silver/27914. 인터뷰/인터뷰.py
@@ -1,63 +1,24 @@
 import sys
 print = sys.stdout.write
 
-
 N,K,Q =  map(int,input().split())
 
-A = list(map(int,input().split()))
+A = list(map(int,input().split())) # len(A) == N
 
 X = list(map(int,input().split()))
 
-subseqeuence = []
-subseqeuence_index = [0]*N
+subseqeuence = [1 if A[i] != K else 0 for i in range(N)]
 
-conti_start = None
-conti_end = None
+A[0] = 1 if A[0] != K else 0
+conti = 1 if A[0] == 1 else 0
 
-for i in range(len(A)):
-    if A[i] == K:
-        if conti_start != None:
-           subseqeuence.append(tuple([conti_start,conti_end]))
-        conti_start = None
+for i in range(1,len(A)):
+    if subseqeuence[i] == 0:
+        A[i] = A[i-1]
+        conti = 0
     else:
-        if conti_start == None:
-            conti_start = i
-        conti_end = i
-    subseqeuence_index[i] = len(subseqeuence)
-else:
-    if conti_start != None:
-        subseqeuence.append(tuple([conti_start,conti_end]))
-
-# print("subseqeuence",subseqeuence)
-# print("subseqeuence_index",subseqeuence_index)
-
-answer_string = ""
+        conti += 1
+        A[i] = A[i-1] + conti
 
 for x in X:
-    ind = subseqeuence_index[x-1]
-    if A[x-1] == K:
-        ind -= 1
-    
-    if ind < 0:
-        # print("0")
-        answer_string += "0\n"
-        continue
-
-    if subseqeuence[ind][1] > x-1:
-        last = (subseqeuence[ind][0],x-1)    
-    else:
-        last = subseqeuence[ind]
-
-    print_answer = 0
-    
-    if ind !=0:
-        for i in range(ind):
-            params = (subseqeuence[i][1] - subseqeuence[i][0])+1
-            print_answer += params*(params+1)//2
-    params = last[1] - last[0] + 1
-    print_answer += params*(params+1)//2
-    # print("x:",x,"ind:",ind,"last:",last,"params:",params,"print_answer:",print_answer)
-    # print(f"{print_answer}\n")
-    answer_string += f"{print_answer}\n"
-print(answer_string)
-        
+    print(f"{A[x-1]}\n")
